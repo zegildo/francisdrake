d0025680ca02eed42cc953cb5f03c8b7e35d5960
Jose Gildo <zegildo@gmail.com>
2019-01-11 15:20:07 -0300

    pylint professors_schedule.py

--- scripts/professors_schedule.py
+++ scripts/professors_schedule.py
@@ -1,78 +1,90 @@
 #coding: utf-8
 """
+Author: ZéGildo (zegildo@gmail.com)
+---------------
 
-cat Desktop/siapes |
-time parallel -j+0 
---progress python Desktop/professors_schedule.py {} > Desktop/horarios.csv_file
+Catching the professors schedule at UFERSA is a annoying task.
+We should to stalk more than 700 personal links, copy and paste
+each detail over many semesters.
+
+To solve this I developed this web crawler that collect the class
+schedule historical for each UFERSA Professor during all his
+professor carrer.
+
 """
+import sys
 import requests
 from lxml import html
-import sys
 
-def buil_schedule_ufersa_professors(URL):
+def get_schedule(nivel):
+    """Get the schedule class present in nivel.
 
-    r = requests.get(URL)
-    hrs = html.fromstring(r.text)
-    niveis = hrs.xpath('//div[@class="aba"]/@id | //td/text() | //td/a/text()')
-    niveis = [e.strip() for e in niveis if e.strip()]
-    
-    PASSO = 4
-    limit = 0
-    periodo = ''
-    turma = ''
-    
-    while limit < (len(niveis)):
-        horario = []
-        if 'turmas-' in niveis[limit]:
-            turma = niveis[limit]
-            limit = limit + 1
-        elif '.' in niveis[limit]:
-            periodo = niveis[limit]
-            horario.append(turma)
-            horario.extend(niveis[limit:limit+PASSO+1])
-            print ','.join(horario).encode('utf-8').strip()
-            horario = []
-            limit = limit + PASSO + 1
-        else:
-            horario.append(turma)
-            horario.append(periodo)
-            horario.extend(niveis[limit:limit+PASSO])
-            print ','.join(horario).encode('utf-8').strip()
-            horario = []
-            limit = limit + PASSO
-    '''    
-    r = requests.get(URL)
-    r.raw.chunked = True
-    hrs = html.fromstring(r.text)
+    Args:
+        nivel: A lxml Element containing the information
+        view (https://lxml.de/api/lxml.etree._Element-class.html).
+
+    Returns:
+        A line with part of the schedule class historical. For example:
+        -------
+        111111,turmas-graduacao,2018.1,PAC0864,ESPAÇO E FORMA,60h,6M2345
+    """
+    nivel = nivel.get('id')
+    ano_periodo = ''
+    codigo_disciplina = ''
+    nome_disciplina = ''
+    carga_horaria = ''
+    horario = ''
+    for element in nivel.iter():
+        if element.tag == 'td':
+            classe = element.get('class')
+            if classe == 'anoPeriodo':
+                ano_periodo = element.text.strip()
+            elif classe == 'codigo':
+                codigo_disciplina = element.text.strip()
+            elif classe == 'ch':
+                carga_horaria = element.text.strip()
+            elif classe == 'horario':
+                horario = element.text.strip()
+                return (SIAPE, nivel, ano_periodo,
+                        codigo_disciplina, nome_disciplina,
+                        carga_horaria, horario)
+        elif element.tag == 'a':
+            nome_disciplina = element.text.strip()
+    return None
+
+def buil_schedule_professor(url):
+    """Get the schedule class from the personal link
+    of a UFERSA's professor
+
+    Args:
+        url: A personal link to a professor's schedule.
+
+    Returns:
+        Multiple lines with the class schedule historical
+        of a professor: For example:
+
+        url = 'https://sigaa.ufersa.edu.br/sigaa/public/docente/disciplinas.jsf?siape=1111111'
+
+        output:
+        -------
+        111111,turmas-graduacao,2018.1,PAC0864,ESPAÇO E FORMA,60h,6M2345
+        111111,turmas-graduacao,2018.1,PSH1601,GEOMETRIA DESCRITIVA,60h,4T2345
+        111111,turmas-graduacao,2018.1,PSH1600,OFICINA DE PLÁSTICA I,60h,6M2345
+        111111,turmas-graduacao,2018.1,PAC0861,PLANEJAMENTO DA PAISAGEM I,60h,6N1234
+    """
+    response = requests.get(url)
+    hrs = html.fromstring(response.text)
     niveis = hrs.xpath('//div[@class="aba"]')
-    
-    for nv in niveis:
-        nivel = nv.get('id')
-        ano_periodo = ''
-        codigo_disciplina = ''
-        nome_disciplina=''
-        carga_horaria = ''
-        horario = ''
-
-        for el in nv.iter():
-            if el.tag == 'td':
-                classe = el.get('class') 
-                if classe == 'anoPeriodo':
-                    ano_periodo = el.text.strip()
-                elif classe == 'codigo':
-                    codigo_disciplina = el.text.strip()
-                elif classe == 'ch':
-                    carga_horaria = el.text.strip()
-                elif classe == 'horario':
-                    horario = el.text.strip()
-                    print (siape,nivel,ano_periodo,
-                            codigo_disciplina,nome_disciplina,
-                            carga_horaria,horario)
-            elif el.tag == 'a':
-                     nome_disciplina = el.text.strip()
-    '''
+    for nivel in niveis:
+        horario = get_schedule(nivel)
+        if horario:
+            print horario
+        else:
+            print 'ERRO:', SIAPE
+#This code should be execute in terminal like:
+#cat siapes | time parallel -j+0 --progress python professors_schedule.py {} > horarios.csv
 if __name__ == "__main__":
 
-    siape = sys.argv[1:][0]
+    SIAPE = sys.argv[1:][0]
     URL = 'https://sigaa.ufersa.edu.br/sigaa/public/docente/disciplinas.jsf?siape='
-    buil_schedule_ufersa_professors(URL+siape)
+    buil_schedule_professor(URL+SIAPE)
